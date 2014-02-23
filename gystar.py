c0e24a4a219d4ba5c592c120bd5067b7e57f7487
Amin Bandali <me@aminbandali.com>
2014-02-22 22:57:17 -0500

    first version of gystar. gets the job done, but not clean at all

---
+++ gystar.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+from datetime import date
+import getpass
+import sys
+sys.path.append("./PyGithub")
+from github import Github
+from github import GithubException
+from github import InputFileContent
+
+
+def main():
+    username = raw_input("Your GitHub username: ")
+    password = getpass.getpass()
+    g = Github(username, password)
+
+    u = g.get_user()
+
+    print "Retreiving your repositories..."
+    repos = u.get_starred()
+    print "done."
+
+    str_gist_body = u"Your starred repositories:\n\n"
+    sys.stdout.write("Trying to build a list of the repositories")
+    sys.stdout.flush()
+    try:
+        for repo in repos:
+            str_gist_body += repo.name + u" " + repo.url + "\n"
+            sys.stdout.write(".")
+            sys.stdout.flush()
+        print
+    except GithubException as ghe:
+        #print "Incorrect username or password."
+        print
+        print ghe
+        sys.exit(1)
+
+    pub_string = raw_input("Make the gist public? [y/N]: ").lower()
+    make_public = len(pub_string) > 0 and pub_string[0] == 'y'
+    files = {'stars': InputFileContent(str_gist_body)}
+    todaydate = str(date.today().strftime("%a %b %d, %Y"))
+    try:
+        u.create_gist(make_public, files,
+                      "A list of your starred repos (generated on "
+                      + todaydate + " )")
+    except GithubException as ghe:
+        print "Sorry, there was a problem creating your gist.",
+        print "Here's the exception:"
+        print ghe
+
+
+main()
